6fe14100b1f5ef2e10e79f01774b24416e9c2c2a
magi954 <utawareikka587@gmail.com>
2021-05-03 07:32:15 +0000

    いいね機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,13 +23,12 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
+    
   end
 
 
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -38,7 +37,6 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path, notice: "successfully delete book!"
   end
